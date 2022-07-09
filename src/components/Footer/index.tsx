bd1e1111bae11dc597e4084aa5ba8aed78d0c555
CTtao <1838346304@qq.com>
2022-07-09 21:47:25 +0800

    Initial commit

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,35 @@
+import { GithubOutlined } from '@ant-design/icons';
+import { DefaultFooter } from '@ant-design/pro-layout';
+import {PLANET_LINK} from "@/constants";
+
+const Footer: React.FC = () => {
+  const defaultMessage = 'CTtao出品';
+  const currentYear = new Date().getFullYear();
+  return (
+    <DefaultFooter
+      copyright={`${currentYear} ${defaultMessage}`}
+      links={[
+        {
+          key: 'planet',
+          title: '知识星球',
+          href: PLANET_LINK,
+          blankTarget: true,
+        },
+        {
+          key: 'codeNav',
+          title: '编程导航',
+          href: 'https://www.code-nav.cn/',
+          blankTarget: true,
+        },
+        {
+          key: 'github',
+          title: <><GithubOutlined /> CT GitHub</>,
+          href: 'https://github.com/CTtao',
+          blankTarget: true,
+        },
+      ]}
+    />
+  );
+};
+
+export default Footer;
